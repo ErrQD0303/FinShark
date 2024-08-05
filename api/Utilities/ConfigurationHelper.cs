a401ee3011065d67c8a8be8016571737ca29ece0
ErrQD0303 <quocdat3396@gmail.com>
2024-08-05 16:58:54 +0700

    Add Database

---
+++ api/Utilities/ConfigurationHelper.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotenv.net;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace api.Utilities
+{
+    public static class ConfigurationHelper
+    {
+        static ConfigurationHelper()
+        {
+            DotEnv.Load();
+        }
+
+        public static string? GetConnectionString(ConfigurationManager configuration)
+        {
+            List<string> envKeys = ["DB_HOST", "DB_NAME", "DB_USER", "DB_PASSWORD"];
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            envKeys.ForEach((key) =>
+            {
+                connectionString = connectionString?.Replace($"{{{key}}}", Environment.GetEnvironmentVariable(key));
+            });
+
+            Console.Write(connectionString);
+
+            // Construct the connection string
+            return connectionString;
+        }
+    }
+}
